748e93c12a022f0d10e8cd546a83b7f172163d43
vikvas <vikvas6@ya.ru>
2020-12-16 15:24:42 +0300

    Lesson 7 HW

---
+++ Unity_CSharp/Assets/Scripts/Editor/CreationWindow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+namespace GeekbrainsUnityCSharp
+{
+    public class CreationWindow : EditorWindow
+    {
+        public static GameObject ObjectToCreate;
+        public Transform StartPosition;
+        public string NameObject = "New object";
+        public bool RandomColor = true;
+        public bool CreateCenterObject = true;
+        public float Size = 1;
+        public int Offset = 10;
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Main settings", EditorStyles.boldLabel);
+            ObjectToCreate = EditorGUILayout.ObjectField("Objects to create", ObjectToCreate, typeof(GameObject), true) as GameObject;
+            NameObject = EditorGUILayout.TextField("Objects Names prefix", NameObject);
+            RandomColor = EditorGUILayout.Toggle("Random color", RandomColor);
+            Size = EditorGUILayout.Slider("Objects size", Size, 0.01f, 25.0f);
+            Offset = EditorGUILayout.IntSlider("Objects offset", Offset, 1, 100);
+            CreateCenterObject = EditorGUILayout.Toggle("Create center?", CreateCenterObject);
+
+            var button = GUILayout.Button("Create objects");
+            if (button)
+            {
+                if (ObjectToCreate)
+                {
+                    Transform root = new GameObject("root").transform;
+                    if (CreateCenterObject)
+                    {
+                        MyCreateObject(0, 0, "(center)", root);
+                    }
+                    MyCreateObject(Offset, Offset, "(1)", root);
+                    MyCreateObject(Offset, -Offset, "(2)", root);
+                    MyCreateObject(-Offset, Offset, "(3)", root);
+                    MyCreateObject(-Offset, -Offset, "(4)", root);
+                }
+            }
+        }
+
+        private void MyCreateObject(float x, float z, string suffix, Transform root)
+        {
+            var pos = new Vector3(x, 0, z);
+            GameObject gameObject = Instantiate(ObjectToCreate, pos, Quaternion.identity);
+            gameObject.transform.localScale = new Vector3(Size, Size, Size);
+            gameObject.name = NameObject + " " + suffix;
+            gameObject.transform.parent = root;
+            var renderer = gameObject.GetComponent<Renderer>();
+            if (renderer && RandomColor)
+            {
+                renderer.material.color = Random.ColorHSV();
+            }
+        }
+    }
+}
---
+++ Unity_CSharp/Assets/Scripts/ObjectsCreatorExt.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+namespace GeekbrainsUnityCSharp
+{
+    [CustomEditor(typeof(ObjectsCreator))]
+    public class ObjectsCreatorExt : UnityEditor.Editor
+    {
+        private bool _isPressButtonOk;
+
+        public override void OnInspectorGUI()
+        {
+            ObjectsCreator targetOC = (ObjectsCreator)target;
+
+            targetOC.ObjectToCreate = EditorGUILayout.ObjectField("Objects to create", targetOC.ObjectToCreate, typeof(GameObject), true) as GameObject;
+            targetOC.NameObject = EditorGUILayout.TextField("Name Prefix", targetOC.NameObject);
+            targetOC.RandomColor = EditorGUILayout.Toggle("Random Color", targetOC.RandomColor);
+            targetOC.Size = EditorGUILayout.Slider("Size", targetOC.Size, 0.01f, 25.0f);
+            targetOC.Offset = EditorGUILayout.IntSlider("Offset", targetOC.Offset, 1, 100);
+
+            var isPressButton = GUILayout.Button("Create objects", EditorStyles.miniButtonLeft);
+
+            _isPressButtonOk = GUILayout.Toggle(_isPressButtonOk, "Show options for the main object");
+
+            if (isPressButton)
+            {
+                targetOC.CreateObjs();
+                _isPressButtonOk = true;
+            }
+
+            if (_isPressButtonOk)
+            {
+                targetOC.Test = EditorGUILayout.Slider(targetOC.Test, 10, 50);
+                EditorGUILayout.HelpBox("The button was pressed", MessageType.Warning);
+
+                var isPressAddButton = GUILayout.Button("Add", EditorStyles.miniButtonLeft);
+                var isPressRemoveButton = GUILayout.Button("Remove", EditorStyles.miniButtonLeft);
+                if (isPressAddButton)
+                {
+                    targetOC.AddComponent();
+                }
+                if (isPressRemoveButton)
+                {
+                    targetOC.RemoveComponent();
+                }
+            }
+        }
+    }
+}
